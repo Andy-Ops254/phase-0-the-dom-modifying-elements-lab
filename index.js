a5ae21111e0e703df89fd0a7b2f11ecf34686f4d
Andy-Ops254 <ghostandy254@outlook.com>
2025-06-21 10:20:27 +0300

    complete DOM lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const removeMain=document.querySelector('#main')
+removeMain.remove()
+console.log(removeMain);
+
+const newHeader=document.createElement('h1')
+newHeader.id='victory'
+newHeader.innerHTML="Andy is the champion"
+document.body.append(newHeader)
+console.log(newHeader)
